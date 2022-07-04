6853f36022a57e2979d20900c240d23d23fbb344
Oluyimide <oluyimidea@gmail.com>
2022-07-04 15:40:45 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,13 +8,13 @@ int main(void)
 {
 	int i = '0';
 	int j = '0';
-	while (i <= '9')
+	while (i <= '8')
 	{
 		while (j <= '9')
 		{
 			putchar(i);
 			putchar(j);
-			if (j == '9' && i == '9')
+			if (j == '8' && i == '9')
 			{
 				putchar('\n');
 			}
@@ -25,7 +25,7 @@ int main(void)
 			}
 			j++;
 		}
-		if (j >= '9')
+		if (j >= '8')
 		{
 			j = '0';
 		}
